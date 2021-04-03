e220606bfbb109f620997f43c3748d182ef3965d
VP <VP>
2021-04-03 18:05:54 +0300

    Initial Commit

---
+++ MVVM_Weather/MVVM_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_WeatherApp.swift
+//  MVVM_Weather
+//
+//  Created by valentine on 03.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
